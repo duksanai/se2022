fb5a365ec6f56859dc9752f61fb7d5a4ba5800be
duksanai <duksanai@gmail.com>
2022-05-26 02:50:15 +0900

    05/26/02:50

--- src/storeUI/ReceiptCheckPage.java
+++ src/storeUI/ReceiptCheckPage.java
@@ -13,6 +13,8 @@ import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollBar;
 import javax.swing.JList;
@@ -21,6 +23,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
+import java.awt.SystemColor;
 
 public class ReceiptCheckPage {
 
@@ -28,7 +31,9 @@ public class ReceiptCheckPage {
 	private JTextField receiptNum;
 	private JPanel panel;
 	private JTextPane receipt;
-	private JScrollPane scrollPane;
+	private JScrollPane receipt_scroll;
+	private JScrollPane receipt_table_scroll;
+	private JTable receipt_table;
 
 	/**
 	 * Launch the application.
@@ -69,26 +74,27 @@ public class ReceiptCheckPage {
 		panel.setLayout(null);
 		
 		receiptNum = new JTextField();
-		receiptNum.setBounds(12, 10, 165, 23);
+		receiptNum.setBounds(12, 10, 138, 23);
 		panel.add(receiptNum);
-		receiptNum.setForeground(Color.LIGHT_GRAY);
+		receiptNum.setForeground(SystemColor.desktop);
 		receiptNum.setColumns(10);
 		
 		JButton Checkbtn = new JButton("조회");
-		Checkbtn.setBounds(189, 10, 57, 23);
+		Checkbtn.setBounds(162, 10, 66, 23);
 		panel.add(Checkbtn);
 		
 		JButton backbtn = new JButton("돌아가기");
-		backbtn.setBounds(258, 10, 81, 23);
+		backbtn.setBounds(240, 10, 99, 23);
 		panel.add(backbtn);
 		
-		scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(12, 61, 351, 270);
-		frame.getContentPane().add(scrollPane);
+		receipt_scroll = new JScrollPane();
+		receipt_scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		receipt_scroll.setBounds(12, 61, 351, 270);
+		frame.getContentPane().add(receipt_scroll);
+		receipt_scroll.setVisible(false);
 		
 		receipt = new JTextPane();
-		scrollPane.setRowHeaderView(receipt);
+		receipt_scroll.setViewportView(receipt);
 		receipt.setText("품명\t\t단가\t수량\t금액\r\n"//영수증 출력
 				+ "---------------------------------------------------------"
 				+ "\r\n과자\t\t1,450\t1\t2,900"
@@ -102,6 +108,56 @@ public class ReceiptCheckPage {
 				+ "\r\n결제수단\t\t\t\t현금결제");
 		receipt.setFont(new Font("굴림", Font.PLAIN, 12));
 		receipt.setEditable(false);
+		
+		receipt_table_scroll = new JScrollPane();
+		receipt_table_scroll.setBounds(12, 61, 351, 270);
+		frame.getContentPane().add(receipt_table_scroll);
+		
+		receipt_table = new JTable();
+		receipt_table_scroll.setViewportView(receipt_table);
+		receipt_table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"영수증 목록"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		receipt_table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2) {
+					receipt_table_scroll.setVisible(false);
+					receipt_scroll.setVisible(true);
+				}
+			}
+		});
 		backbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Main_UI mp = new Main_UI();
